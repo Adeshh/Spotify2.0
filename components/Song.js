7b5d431a3a78731fdac49ec57c70486a3fa5121c
chiragbadhe <badhechirag633@gmail.com>
2023-01-23 01:57:47 +0530

    Fix: Props

--- components/Song.js
+++ components/Song.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import useSpotify from '../hooks/useSpotify'
-import { useEffect } from 'react';
+import React from "react";
+import useSpotify from "../hooks/useSpotify";
+import { useEffect } from "react";
 
-const Song = (order, track ) => {
-    const spotifyApi = useSpotify();
-    //console.log(track)
-  
+const Song = ({ track }) => {
+  console.log(track);
+
   return (
     <div>
-        <div>
-            <p>{order.order + 1 } </p>
-            {/* //<p>{track}</p> */}
-            {/* //<img src={track.track.album.images[0].url} alt="" /> */}
+      <div>
+        {/* //<p>{track}</p> */}
+        <div className="flex overflow-y-scroll overflow-scroll ">
+          <img src={track.track.album.images[0].url} alt="" />
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Song
+export default Song;
--- components/Songs.js
+++ components/Songs.js
@@ -2,16 +2,15 @@ import React from 'react'
 import { useRecoilValue } from 'recoil'
 import { playlistIdState, playlistState } from '../atoms/playlistAtom';
 import Song from '../components/Song'
+import { useState } from 'react';
 
 const Songs = () => {
     const playlist = useRecoilValue(playlistState)
-
+    
   return (
     <div className='text-white px-8 flex flex-col space-y-1 pb-28 '>
         {playlist?.tracks.items.map((track, i) =>(
-           console.log(track.track),
-            // <div key={track.track.name}>{track.track.name}</div>
-            <Song key={track.track.id} track={track.track} order={i} />
+            <Song track={track} />
         ))}
     </div>
   )
